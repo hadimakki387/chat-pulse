ff5f16929820673b93e9e4524438387ca2c72e2b
hadimakki387 <hmakki387@gmail.com>
2023-06-12 19:31:51 +0300

    set a link for the auth window

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,10 +6,7 @@ export default function Home() {
       <Head>
         <title>Next JS ChatGPT Starter</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <Link href={"/api/auth/login"}>
-        Login
-      </Link>
+      <h1>Welcome to the Next JS page</h1>
     </div>
   );
 }
