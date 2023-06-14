b00a67bb0b3d233ee9e7532fcf49168c77cd5755
hadimakki387 <hmakki387@gmail.com>
2023-06-14 18:37:28 +0300

    set the grid layout

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -2,11 +2,23 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <div className="h-screen w-full">
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>New Chat</title>
       </Head>
-      <h1>Welcome to the Next JS page</h1>
+      <div className="grid h-full grid-cols-[250px_1fr]">
+          <div className="">
+          side bar
+          </div>
+          <div className="bg-gray-700 grid grid-rows-[1fr_100px]">
+            <div>
+              hello
+            </div>
+            <footer className="bg-gray-800 p-10">
+              nigger
+            </footer>
+          </div>
+      </div>
     </div>
   );
 }
