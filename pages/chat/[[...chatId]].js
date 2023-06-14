4a55c040cab46ef4391d9675003045a565045f8e
hadimakki387 <hmakki387@gmail.com>
2023-06-14 19:21:37 +0300

    created the form for the messages

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,17 +1,39 @@
 import ChatSideBar from "components/ChatSideBar/ChatSideBar";
 import Head from "next/head";
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(message)
+  }
+  
   return (
     <div className="h-screen w-full">
       <Head>
         <title>New Chat</title>
       </Head>
       <div className="grid h-full grid-cols-[250px_1fr]">
-        <ChatSideBar/>
+        <ChatSideBar />
         <div className="grid grid-rows-[1fr_100px] bg-gray-700">
           <div>hello</div>
-          <footer className="bg-gray-800 p-10">nigger</footer>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                placeholder="Send a message ..."
+                className="w-full resize-none rounded-md bg-gray-700  text-white focus:border-emerald-500  focus:outline focus:outline-emerald-500"
+                onChange={e=>setMessage(e.target.value)}
+
+                />
+
+                <button type="submit" className="btn">
+                  send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </div>
