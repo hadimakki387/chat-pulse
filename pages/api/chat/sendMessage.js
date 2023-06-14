2e737a16a86a266543d4ac24f5291eb24525cd5c
hadimakki387 <hmakki387@gmail.com>
2023-06-14 19:53:41 +0300

    created sendMessage file

---
+++ pages/api/chat/sendMessage.js
@@ -0,0 +1,35 @@
+import { headers } from "next/dist/client/components/headers";
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req) {
+  try {
+    const { message } = await req.json();
+    const stream = await OpenAIEdgeStream(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              content: message,
+              role: "user",
+            },
+          ],
+          stream: true,
+        }),
+      }
+    );
+    return new Response(stream)
+  } catch (e) {
+    console.log(e);
+  }
+}
